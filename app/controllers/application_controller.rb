a653e7b115546233df0531e3a87971d839577239
Christophe Robillard <geek@robiweb.net>
2013-06-30 12:31:01 +0200

    set up internationalisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  before_filter :set_locale
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
 
+  private
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Edgar::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/  do
+    authenticated :user do
+      root :to => 'home#index', :as => :locale_root
+    end
+    devise_scope :user do
+      root :to => "devise/registrations#new"
+      match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    end
+    devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
+    match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
+    resources :users do
+      get 'invite', :on => :member
+    end
   end
-  devise_scope :user do
-    root :to => "devise/registrations#new"
-    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
-  end
-  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
-  match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
-  resources :users do
-    get 'invite', :on => :member
-  end
+
+# handles /
+  root to: redirect("/#{I18n.default_locale}")
 end
