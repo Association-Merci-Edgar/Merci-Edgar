9990bcdc894c22f7eb81d74b29f61007f6016735
Jean-Paul Bagnis <jpbagnis@gmail.com>
2013-08-26 11:10:15 +0200

    build VENUE INDEX, better CSS better ONEPAGE LOGIN

--- app/assets/javascripts/edgarapp.js
+++ app/assets/javascripts/edgarapp.js
@@ -1,52 +1,45 @@
-
 $('document').ready(function() {
 	
-	// show/hide the leftpanel _________________________________
-	window.smallScreens = function() {
-		if ($(window).width() < 767) {
+// show/hide the leftpanel _________________________________
+  window.smallScreens = function() {
+    $('.leftpanel').removeClass( "collapsed");
+		$('.mainpanel').removeClass( "goleft");
+    $('.leftpanel').removeClass( "small");
+		$('.mainpanel').removeClass( "goleftsmall");
+		
+    if ($(window).width() <= 767) {
 			$('.leftpanel').addClass( "collapsed");
 			$('.mainpanel').addClass( "goleft");
-		} else {
-			$('.leftpanel').removeClass( "collapsed");
-			$('.mainpanel').removeClass( "goleft");
+			
+    
+			
+		} else if ($(window).width() <= 1024) {
+  			$('.leftpanel').addClass( "small");
+  			$('.mainpanel').addClass( "goleftsmall");
+			
+		} else if ($(window).width() > 1024){
+			
 		}
+		
+		
 	}
 	
-  /*$('#nav_toggle').click(function(){
-		$('.leftpanel').animate({scrollTop:0}, 'slow');	
-		return false;
-	});
-  */
+  
 	$('.leftpanel.collapsed').animate();
 	$('.leftpanel:not(.collapsed)').animate();
 
 	$("#nav_toggle").click(function () {
-/*$(".leftpanel").toggle("slow");*/
+    /*$(".leftpanel").toggle("slow");*/
 		$('.leftpanel').toggleClass( "collapsed");
 		$('.mainpanel').toggleClass( "goleft");
 	});
+	
+	
+	
 	//alert ("yo");
 	smallScreens();
-	
-	// Clickable Dropdown _________________________________
-	//$(function() {
-  			
-  			$('.click-nav > .ul').toggleClass('no-js js');
-  			$('.click-nav .js ul').hide();
-  			$('.click-nav .js').click(function(e) {
-  				$('.click-nav .js ul').slideToggle(200);
-  				$('.clicker').toggleClass('active');
-  				e.stopPropagation();
-  			});
-  			$(document).click(function() {
-  				if ($('.click-nav .js ul').is(':visible')) {
-  					$('.click-nav .js ul', this).slideUp(0);
-  					$('.clicker').removeClass('active');
-  				}
-  			});
-  		//});
-	
 })
+
 
 
 $(window).load(function() {
@@ -57,3 +50,26 @@ $(window).resize(function() {
 	smallScreens();	
 });
 
+
+
+// Clickable Dropdown _________________________________
+$(function() {
+      $('.clicker').click(function(event) { event.preventDefault();});
+			$('.click-nav > .ul').toggleClass('no-js js');
+			$('.click-nav .js ul').hide();
+			$('.click-nav .js').click(function(e) {
+				$('.click-nav .js ul').slideToggle(200);
+				$('.clicker').toggleClass('active');
+				e.stopPropagation();
+			});
+			
+			$(document).click(function() {
+				if ($('.click-nav .js ul').is(':visible')) {
+					$('.click-nav .js ul', this).slideUp(0);
+					$('.clicker').removeClass('active');
+				}
+			});
+});
+
+//_______
+		
