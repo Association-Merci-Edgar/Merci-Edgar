de8e8bf452fc8b40e489337f5e82779658cda96b
Christophe Robillard <geek@robiweb.net>
2013-08-30 21:27:52 +0200

    fix label_tooltip

--- config/initializers/simple_form/label_tooltip_component.rb
+++ config/initializers/simple_form/label_tooltip_component.rb
@@ -9,15 +9,17 @@ module SimpleForm
 
       def label_tooltip
         if options[:label] != false
-          if options[:tooltip].present?
+          if options[:tooltip] != false
             tooltip = options[:tooltip]
             tooltip_content = tooltip.is_a?(String) ? tooltip : translate(:tooltips)
-            tooltip_content.html_safe if tooltip_content
-            html = template.content_tag(:a, href:"#", :"data-toggle" => "tooltip", :"title" => tooltip_content) do
-              template.content_tag(:i, '', :class => 'entypo edi-help')
+            if tooltip_content
+              tooltip_content.html_safe
+              html = template.content_tag(:a, href:"#", :"data-toggle" => "tooltip", :"title" => tooltip_content) do
+                template.content_tag(:i, '', :class => 'entypo edi-help')
+              end
             end
           end
-          
+
           if html.present?
             @builder.label(label_target, label_html_options) do
               html += label_text
