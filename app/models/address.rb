293b5c5bc6532e0f5d55226a4285b6829af9913a
Christophe Robillard <geek@robiweb.net>
2013-09-04 16:59:06 +0200

    change address, _person and _venue for index contacts

--- app/models/address.rb
+++ app/models/address.rb
@@ -28,6 +28,7 @@ class Address < ActiveRecord::Base
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     "#{self.street}, #{self.city}, Country.new(#{self.country}).name"
+    [self.street, self.city, Country.new(self.country).name].reject(&:blank?).join(', ')
   end
 
 
