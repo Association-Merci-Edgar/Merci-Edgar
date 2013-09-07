08c33395c6ca4e4da97cc9e20ceef1e4e5973249
Jean-Paul Bagnis <jpbagnis@gmail.com>
2013-09-07 13:39:40 +0200

    EMPTY sreens Notices CLASS SHOW VENUE better CSS

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,14 @@
 class ContactsController < AppController
   def index
-    if params[:tag]
-      @filtered_by = "(#{params[:tag]})"
-      @contacts = Kaminari.paginate_array(Contact.tagged_with(params[:tag])).page params[:page]
+    if params[:empty] == "1"
+      render "empty"
     else
-      @contacts = Contact.search(params[:search]).order(:name).page params[:page]
+      if params[:tag]
+        @filtered_by = "(#{params[:tag]})"
+        @contacts = Kaminari.paginate_array(Contact.tagged_with(params[:tag])).page params[:page]
+      else
+        @contacts = Contact.search(params[:search]).order(:name).page params[:page]
+      end
     end
   end
 end
