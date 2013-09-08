3c89693acaf9c893ab60ffed5aef0180e5e4c186
Christophe Robillard <geek@robiweb.net>
2013-09-08 20:13:00 +0200

    update profile and account edit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def user_tab_link(link_text, link, controller)
     class_name = controller_name == controller ? "active" : ""
-    link_to link_text, link, class: class_name
+    content_tag(:li,link_to(link_text, link), class: class_name)
   end
 end
