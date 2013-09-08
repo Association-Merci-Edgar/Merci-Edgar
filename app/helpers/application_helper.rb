6fc906011af7d534eaed7473b9fb1af6ab65e461
Christophe Robillard <geek@robiweb.net>
2013-09-08 20:47:15 +0200

    left panel with active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,10 +15,21 @@ module ApplicationHelper
   def previous
     @previous = @previous.blank? ? request.env['HTTP_REFERRER'] : @previous
   end
-  
+
   def display_errors(object)
     if object && object.respond_to?(:errors) && object.errors.present?
       content_tag(:div,"Oups ! Y a des soucis !",class:'alert alert-error')
     end
   end
+
+  def menu_link(link_text, link, icon, controller, badge)
+    class_name = controller_name == controller ? "active" : ""
+    badge_tag = content_tag(:span,badge, class:"badge")
+    link_tag = link_to link do
+      content_tag(:i,nil,class:"entypo #{icon}") + " " + link_text + badge_tag
+    end
+    content_tag(:li,link_tag,class: class_name)
+  end
 end
+
+
