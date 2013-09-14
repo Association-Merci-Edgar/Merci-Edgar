d348df82d6446c03e636e970c9fd6c540c385760
Christophe Robillard <geek@robiweb.net>
2013-09-14 11:40:40 +0200

    add autocomplete in person form

--- app/controllers/structures_controller.rb
+++ app/controllers/structures_controller.rb
@@ -4,6 +4,6 @@ class StructuresController < ApplicationController
     @structures = Venue.order(:name).where("name LIKE ?", "%#{params[:term]}%")
     json=[]
     @structures.each { |s| json.push({id:s.id, name:s.name, city:s.city, country:s.country}) }
-    render json: json
+    render json: @structures.map(&:name)
   end
 end
