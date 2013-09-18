82b11191dda886251bdcd7d4457297985a0a2c90
Christophe Robillard <geek@robiweb.net>
2013-09-18 13:42:12 +0200

    fix migrations

--- db/migrate/20130917132546_create_rooms.rb
+++ db/migrate/20130917132546_create_rooms.rb
@@ -1,9 +1,9 @@
 class CreateRooms < ActiveRecord::Migration
-  def change
-    remove_column :venue_infos, :depth, :float
-    remove_column :venue_infos, :width, :float
-    remove_column :venue_infos, :height, :float
-    remove_column :venue_infos, :bar, :boolean
+  def up
+    remove_column :venue_infos, :depth
+    remove_column :venue_infos, :width
+    remove_column :venue_infos, :height
+    remove_column :venue_infos, :bar
     create_table :rooms do |t|
       t.string :name
       t.float :depth
@@ -15,8 +15,18 @@ class CreateRooms < ActiveRecord::Migration
       t.timestamps
     end
     add_index :rooms, :venue_id
-    remove_column :capacities, :venue_info_id, :integer
+    remove_column :capacities, :venue_info_id
     add_column :capacities, :room_id, :integer
     add_index :capacities, :room_id
   end
+  def down
+    add_column :venue_infos, :depth, :float
+    add_column :venue_infos, :width, :float
+    add_column :venue_infos, :height, :float
+    add_column :venue_infos, :bar, :boolean
+    drop_table :rooms
+    add_column :capacities, :venue_info_id, :integer
+    remove_column :capacities, :room_id
+  end
+
 end
