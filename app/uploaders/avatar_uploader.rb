d5f40031ee396c41b3c8ffd9ebf2a23db1f3cca7
Christophe Robillard <geek@robiweb.net>
2013-09-24 20:52:28 +0200

    avatar in fog, change _tags, delete all local uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,8 +9,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process :set_content_type
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
