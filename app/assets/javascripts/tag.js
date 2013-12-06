b3ccfcbc268c2963e6580a538ad0d2e288255b70
Christophe Robillard <geek@robiweb.net>
2013-12-06 16:38:28 +0100

    autocomplete for style input only, without tagit

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -1,81 +1,102 @@
-/*
-$.ajax({
-    type: 'get',
-    url: '/tags/index?type=CustomTag',
-    data: "",
-    dataType: 'json',
-    success: function(data) {
-        var sampleTags = data;
-        $('.autocomplete_tags').tagit({
-            availableTags: sampleTags,
-            singleField: true,
-            singleFieldNode: $('#mySingleField'),
-            allowSpaces: true
-        });
+$(function() {
+    function split( val ) {
+      return val.split( /,\s*/ );
     }
-})
-*/
-
-$(document).ready(function() {
-
-	var availableStyleTags = $(".style_tags_input").data('autocomplete-source')
-  $(".style_tags_input").tagit({
-		singleField: true,
-		allowSpaces: true,
-		// availableTags: availableStyleTags
-		autocomplete: {
-	    source: availableStyleTags,
-	    delay: 1000
-	  }	
-	});
-
-	var availableNetworkTags = $(".network_tags_input").data('autocomplete-source')
-  $(".network_tags_input").tagit({
-		singleField: true,
-		allowSpaces: true,
-		autocomplete: {
-	    source: availableNetworkTags,
-	    delay: 1000
-	  }	
-	});
-
-	var availableCustomTags = $(".custom_tags_input").data('autocomplete-source')
-  $(".custom_tags_input").tagit({
-		singleField: true,
-		allowSpaces: true,
-		autocomplete: {
-	    source: availableCustomTags,
-	    delay: 1000
-	  }	
-	});
-
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+ 
+		var availableStyleTags = $(".style_tags_input").data('autocomplete-source')
+    $( ".style_tags_input" )
+      // don't navigate away from the field on tab when selecting an item
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).data( "ui-autocomplete" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        source: function( request, response ) {
+          $.getJSON( availableStyleTags, {
+            term: extractLast( request.term )
+          }, response );
+        },
+        search: function() {
+          // custom minLength
+          var term = extractLast( this.value );
+          if ( term.length < 2 ) {
+            return false;
+          }
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+      });
 });
 
+
+
 $(document).on('nested:fieldAdded', function(event){
+	function split( val ) {
+    return val.split( /,\s*/ );
+  }
+  function extractLast( term ) {
+    return split( term ).pop();
+  }
+
   var field = event.field;
 	input = field.find('.style_tags_input')
 
 	var availableStyleTags = input.data('autocomplete-source')
-  input.tagit({
-		singleField: true,
-		allowSpaces: true,
-		//availableTags: availableStyleTags
-		
-		
-		autocomplete: {
-	    source: availableStyleTags,
-	    delay: 1000
-			// _renderItem: function(ul, item) { return $("<li>").append("<a>" + item.label + "</a>").appendTo(ul) }
-    }
-	
-	});
-/*	
-	input.autocomplete({
-    source: availableStyleTags,
-    delay: 1000
-		
-	}).data("ui-autocomplete")._renderItem = function(ul, item) {
-    return $("<li>").append("<a>" + item.label + "</a>").appendTo(ul);
-  };
-*/  
+	input
+    // don't navigate away from the field on tab when selecting an item
+    .bind( "keydown", function( event ) {
+      if ( event.keyCode === $.ui.keyCode.TAB &&
+          $( this ).data( "ui-autocomplete" ).menu.active ) {
+        event.preventDefault();
+      }
+    })
+    .autocomplete({
+      source: function( request, response ) {
+        $.getJSON( availableStyleTags, {
+          term: extractLast( request.term )
+        }, response );
+      },
+      search: function() {
+        // custom minLength
+        var term = extractLast( this.value );
+        if ( term.length < 2 ) {
+          return false;
+        }
+      },
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      select: function( event, ui ) {
+        var terms = split( this.value );
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push( "" );
+        this.value = terms.join( ", " );
+        return false;
+      }
+    }).data("ui-autocomplete")._renderItem = function(ul, item) {
+			return $("<li>").append("<a>" + item.label + "</a>").appendTo(ul);  
+		};
 });
