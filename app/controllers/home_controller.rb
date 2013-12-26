7d1b064f5fa3e295a1e4686292d73b050e4794da
Christophe Robillard <geek@robiweb.net>
2013-12-26 11:00:47 +0100

    clean logs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,10 @@
 class HomeController < AppController
   def index
-    puts "in home controller !!!!!!!!!!!!!"
     if current_user.has_role? :admin
       redirect_to users_path
     else
 
-      puts "welcome hidden ?"
       if !current_user.welcome_hidden?
-        puts "redirect to welcome"
         redirect_to welcome_path
         return
       end
