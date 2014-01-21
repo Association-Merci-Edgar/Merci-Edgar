91a4f8e5c7260f8d2ae7cf8d74e01915070cceb1
Christophe Robillard <geek@robiweb.net>
2014-01-22 00:33:10 +0100

    downcase tags update

--- db/migrate/20140115174552_downcase_all_tags.rb
+++ db/migrate/20140115174552_downcase_all_tags.rb
@@ -5,15 +5,15 @@ class DowncaseAllTags < ActiveRecord::Migration
     Custom.unscoped.destroy_all
     Venue.unscoped.find_each do |v|
       Account.current_id = v.account_id
-      v.schedulings.find_each {|s| s.format_styles; s.update_styles; s.save}
+      v.schedulings.each {|s| s.format_styles; s.update_styles; s.save}
     end
     Festival.unscoped.find_each do |v|
       Account.current_id = v.account_id
-      v.schedulings.find_each {|s| s.format_styles; s.update_styles; s.save}
+      v.schedulings.each {|s| s.format_styles; s.update_styles; s.save}
     end
     ShowBuyer.unscoped.find_each do |v|
       Account.current_id = v.account_id
-      v.schedulings.find_each {|s| s.format_styles; s.update_styles; s.save}
+      v.schedulings.each {|s| s.format_styles; s.update_styles; s.save}
     end
 
     Contact.unscoped.find_each {|c| Account.current_id=c.account_id; c.format_customs; c.format_networks; c.update_customs; c.update_networks; c.save}
