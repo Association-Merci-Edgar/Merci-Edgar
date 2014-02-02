960f9757f67dab8bf0d0ee5bd9fbb460bafe72ca
Christophe Robillard <geek@robiweb.net>
2014-02-02 21:53:46 +0100

    fix prospecting migration, update info scheduling

--- db/migrate/20140202132335_add_prospecting_months_to_schedulings.rb
+++ db/migrate/20140202132335_add_prospecting_months_to_schedulings.rb
@@ -1,10 +1,18 @@
 class AddProspectingMonthsToSchedulings < ActiveRecord::Migration
-  def change
+  def up
     add_column :schedulings, :prospecting_months, :string_array
+    Scheduling.reset_column_information
     Scheduling.find_each do |s|
       months = []
       s.prospectings.each {|p| months.push(*ModuloRange.new(p.start_month,p.end_month))}
-      s.update_attributes(:prospecting_months => months) if months.present?
+      if months.present?
+        s.update_attributes!(:prospecting_months => months.map(&:to_s))
+        puts "#{s.id} // months added: #{s.prospecting_months}"
+      end
     end    
+  end
+  
+  def down
+    remove_column :schedulings, :prospecting_months
   end
 end
