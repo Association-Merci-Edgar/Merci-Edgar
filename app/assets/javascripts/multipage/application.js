590071f67e55603ed2d64f413f4c18d25072953c
Jean-Paul Bagnis <jpbagnis@gmail.com>
2014-02-12 18:02:29 +0100

    Multipage showcase V1

--- app/assets/javascripts/multipage/application.js
+++ app/assets/javascripts/multipage/application.js
@@ -16,4 +16,90 @@
 //= require bootstrap
 //= require_self
 
+jQuery('document').ready(function() {
 
+  // display validation errors for the "request invitation" form
+  if (jQuery('.alert-error').length > 0) {
+    jQuery("#request-invite").modal('toggle');
+  }
+
+  // use AJAX to submit the "request invitation" form
+  jQuery('#invitation_button').on('click', function() {
+    email = jQuery('#invit_user #user_email').val();
+    dataString = 'user[email]='+ email;
+    jQuery.ajax({
+      type: "POST",
+      url: "/fr/users",
+      data: dataString,
+      success: function(data) {
+        jQuery('#request-invite').html(data);
+        loadSocial();
+      }
+    });
+    return false;
+  });
+
+})
+
+// load social sharing scripts if the page includes a Twitter "share" button
+function loadSocial() {
+
+    //Twitter
+    if (typeof (twttr) != 'undefined') {
+      twttr.widgets.load();
+    } else {
+      jQuery.getScript('http://platform.twitter.com/widgets.js');
+    }
+
+    //Facebook
+    if (typeof (FB) != 'undefined') {
+      FB.init({ status: true, cookie: true, xfbml: true });
+    } else {
+      jQuery.getScript("http://connect.facebook.net/en_US/all.js#xfbml=1", function () {
+        FB.init({ status: true, cookie: true, xfbml: true });
+      });
+    }
+
+    //Google+
+    if (typeof (gapi) != 'undefined') {
+      jQuery(".g-plusone").each(function () {
+        gapi.plusone.render($(this).get(0));
+      });
+    } else {
+      jQuery.getScript('https://apis.google.com/js/plusone.js');
+    }
+}
+
+
+
+jQuery('.login-toggle').click(function(){
+  console.log("ter")
+  if (!jQuery('#login-tab').hasClass("invisible")) {
+    jQuery('#login-tab').addClass("invisible");
+    jQuery('#nav').addClass("hidden");
+    //jQuery('#socialfixed').addClass("invisible");
+    //jQuery('#langswitch').addClass("invisible");
+    jQuery('#login-form').removeClass("invisible");
+    jQuery("input:text:visible:first").focus();
+  } else {
+    jQuery('#login-tab').removeClass("invisible");
+    jQuery('#nav').removeClass("hidden");
+    //jQuery('#socialfixed').removeClass("invisible");
+    //jQuery('#langswitch').removeClass("invisible");
+    jQuery('#login-form').addClass("invisible");
+  }
+  
+});
+
+
+jQuery("[rel='tooltip']").tooltip();
+
+
+
+jQuery('.login-btn').removeClass("btn");
+
+jQuery('.nav-collapse .nav > li > a').click(function(){
+			
+			jQuery('.collapse.in').removeClass('in').css('height', '0');
+
+});
