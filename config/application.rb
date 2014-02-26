48992d1aa360d559955392594f1f1cf1d0f563d0
Christophe Robillard <geek@robiweb.net>
2014-02-26 20:41:57 +0100

    add config.locale for heroku

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,7 @@ module Edgar
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
+    config.i18n.locale = :fr
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
