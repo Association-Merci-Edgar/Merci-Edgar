0667ada7c0bc9420502decf5a440740bc926fd6b
Christophe Robillard <geek@robiweb.net>
2014-05-07 09:52:55 +0200

    initiate test_mode_show view

--- app/controllers/contacts_imports_controller.rb
+++ app/controllers/contacts_imports_controller.rb
@@ -10,8 +10,7 @@ class ContactsImportsController < AppController
       uploader = ContactsImportUploader.new(Account.current_id.to_s)
       uploader.store!(@import.contact_file)
       @job_id = ContactsImportWorker.perform_async(Account.current_id, @import.filename, @import.options)
-       #if @import.test_mode == true
-      redirect_to contacts_import_path(@job_id)
+      redirect_to contacts_import_path(@job_id, test_mode: @import.test_mode)
     else
       render "errors"
     end
@@ -23,5 +22,8 @@ class ContactsImportsController < AppController
     @nb_imported_contacts = status_container.payload["nb_imported_contacts"]
     @nb_duplicates = status_container.payload["nb_duplicates"]
     @imported_at = status_container.payload["imported_at"]
+    if params[:test_mode] == "true"
+      render action: "test_mode_show"
+    end
   end
 end
