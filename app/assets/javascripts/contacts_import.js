b8cfe3a608073f0277a9672b9fd029dec8986865
Christophe Robillard <geek@robiweb.net>
2014-06-12 20:24:53 +0200

    merge import.js in contacts_import.js

--- app/assets/javascripts/contacts_import.js
+++ app/assets/javascripts/contacts_import.js
@@ -1,3 +1,17 @@
+$('#contact-file-type input.radio_buttons').on('click', function (event) {
+  //event.stopPropagation(); event.preventDefault();
+  //$('.new_reporting textarea').val("");
+  if ( $(this).closest('span').hasClass('xml') ) {
+    console.log("xml");
+    //$('#first_name_last_name').addClass('hidden');
+    $('#first_name_last_name').slideUp(200);
+  } else {
+    //$('#first_name_last_name').removeClass('hidden');
+    $('#first_name_last_name').slideDown(200);
+  }
+  
+});
+
 function pollJobidStatus() {
 	var progressBarElement, progressBarSection, barElement, messageElement;
 	progressBarElement = $("#progress_import")
