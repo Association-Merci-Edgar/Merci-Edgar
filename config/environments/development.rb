2f597785a6ddf3059d094aae95da6a7d28dae9cd
Christophe Robillard <geek@robiweb.net>
2014-06-25 18:07:59 +0200

    log for setting pool size sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,7 @@ Edgar::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.log_level = :debug
+  
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,8 +8,15 @@ Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
 
   database_url = ENV['DATABASE_URL']
-  if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=8"
+  sidekiq_concurrency = ENV['SIDEKIQ_CONCURRENCY']
+  if(database_url && sidekiq_concurrency)
+    Rails.logger.info("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
+
+    ENV['DATABASE_URL'] = "#{database_url}?pool=#{sidekiq_concurrency}"
+    Rails.logger.info(%Q(DATABASE_URL => "#{ENV['DATABASE_URL']}"))
+
     ActiveRecord::Base.establish_connection
   end
+
+  Rails.logger.info("Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
 end
