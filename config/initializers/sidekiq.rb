b2ceff4de902ba8097b00535a65d8b1d3baee320
Christophe Robillard <geek@robiweb.net>
2014-06-25 15:03:38 +0200

    update pool size, workers for unicorn, and pool for sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+ENV["REDIS_URL"] ||= ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
+
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=8"
+    ActiveRecord::Base.establish_connection
+  end
+end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,8 +20,14 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                    Rails.application.config.database_configuration[Rails.env]
+    # config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds # For Rails 4
+    config['pool']            =   ENV['DB_POOL'] || 2
+    
+    ActiveRecord::Base.establish_connection(config)
+  end
 =begin
   Sidekiq.configure_client do |config|
     config.redis = { :size => 1 }
