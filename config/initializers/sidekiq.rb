1a34a431b533313a1777906e1e68bb33173e56ac
Christophe Robillard <geek@robiweb.net>
2014-06-25 16:46:22 +0200

    change sidekiq client initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
 ENV["REDIS_URL"] ||= ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
 
