32dd442a44ca94ba4a71da857f505c6e747df81d
Christophe Robillard <geek@robiweb.net>
2014-06-30 21:26:25 +0200

    update debug messages in sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,7 +11,7 @@ Sidekiq.configure_server do |config|
   sidekiq_concurrency = ENV['SIDEKIQ_CONCURRENCY']
   db_pool_size = Integer(sidekiq_concurrency) + 2
   if(database_url && sidekiq_concurrency)
-    Rails.logger.debug("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
+    Rails.logger.debug("Setting custom connection pool size of #{db_pool_size} for Sidekiq Server")
 
     ENV['DATABASE_URL'] = "#{database_url}?pool=#{db_pool_size}" 
     Rails.logger.info(%Q(DATABASE_URL => "#{ENV['DATABASE_URL']}"))
