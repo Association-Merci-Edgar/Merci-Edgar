d26bdf1ccd0e94b61d027e62d0c42908bfdcaeb4
Christophe Robillard <geek@robiweb.net>
2014-06-30 14:44:01 +0200

    add connections for sidekiq db pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,10 +9,11 @@ Sidekiq.configure_server do |config|
 
   database_url = ENV['DATABASE_URL']
   sidekiq_concurrency = ENV['SIDEKIQ_CONCURRENCY']
+  db_pool_size = Integer(sidekiq_concurrency) + 2
   if(database_url && sidekiq_concurrency)
-    Rails.logger.info("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
+    Rails.logger.debug("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
 
-    ENV['DATABASE_URL'] = "#{database_url}?pool=#{sidekiq_concurrency}"
+    ENV['DATABASE_URL'] = "#{database_url}?pool=#{sidekiq_concurrency}" 
     Rails.logger.info(%Q(DATABASE_URL => "#{ENV['DATABASE_URL']}"))
 
     ActiveRecord::Base.establish_connection
