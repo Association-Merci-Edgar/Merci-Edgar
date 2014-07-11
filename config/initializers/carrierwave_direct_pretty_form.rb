e5e7b64c38220ef4e03e77631adfe08c98a52989
Christophe Robillard <geek@robiweb.net>
2014-07-11 17:50:26 +0200

    pretty input file for carrierwave_direct

---
+++ config/initializers/carrierwave_direct_pretty_form.rb
@@ -0,0 +1,24 @@
+module CarrierWaveDirect
+  class FormBuilder < ActionView::Helpers::FormBuilder
+    def file_field(method, options = {})
+      options.merge!(:name => "file")
+      fields = required_base_fields
+      fields << content_type_field(options)
+      fields << success_action_field(options)
+      
+      idf = [object_name, method.to_s].join("_")
+      options[:style] ||= 'display:none;'
+
+      button = @template.content_tag(:div, class: 'input-append') do 
+        @template.tag(:input, id: "pbox_#{idf}", class: 'string input-medium', :style => "display:none", type: 'text') +
+        @template.content_tag(:a, I18n.t('helpers.browse'), class: 'btn btn-secondary', onclick: "$('input[id=#{idf}]').click();")
+      end
+
+      script = @template.content_tag(:script, type: 'text/javascript') do
+        "$('input[id=#{idf}]').change(function() { s = $(this).val(); $('#pbox_#{idf}').val(s.slice(s.lastIndexOf('\\\\')+1));$('#pbox_#{idf}').show() });".html_safe
+      end
+
+      fields << super + button + script
+    end
+  end
+end
