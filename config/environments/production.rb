f515eb7f9a14c7dbaddcd0513e19bd2407b74d5d
Yannick Francois <yannick.francois@free.fr>
2015-04-29 18:41:02 +0200

    Try something

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Edgar::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -73,7 +73,7 @@ Edgar::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.default :charset => "utf-8"
 
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
