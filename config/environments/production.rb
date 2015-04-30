156770b5dcdc734169d10360a44ca0b5a10fda62
Yannick Francois <yannick.francois@free.fr>
2015-04-30 16:30:57 +0200

    serve static asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,10 +8,7 @@ Edgar::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
+  config.serve_static_assets = true
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
