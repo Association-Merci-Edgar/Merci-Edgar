47bc4f3dd3e140dbd427c9ed811d0ec2a1608181
Yannick Francois <yannick.francois@free.fr>
2015-05-02 23:05:44 +0200

    Regenerate assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,62 +1,36 @@
 Edgar::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
+
+  config.serve_static_assets = false
+
   config.assets.enabled = true
-  #config.assets.compress = true
-  #config.assets.css_compressor = :yui
-  #config.assets.js_compressor = :uglifier
- # config.assets.digest = true
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.digest = true
 
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  config.assets.precompile += %w( jquery.js jquery_ujs contacts_index.js contacts.js tag.js contacts_form.js scheduling.js tasks_group.js simple/application.js onepage/application.js tour/home-tour-fr.js onepage.js onepage.css .svg .eot .woff .ttf multipage.css )
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable threaded mode
-  # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => ENV['DOMAIN'] }
-  # ActionMailer Config
-  # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-    config.action_mailer.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 25,
-      :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_API_KEY"]
-    }
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }
 
-
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
