8095f12f75f0ec1924b26ad9f8d7df29027a2fe0
Yannick Francois <yannick.francois@free.fr>
2015-05-03 16:54:10 +0200

    remove commented code

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,6 @@ module Edgar
 
     config.active_record.whitelist_attributes = true
 
-    config.assets.enabled = true
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
 
