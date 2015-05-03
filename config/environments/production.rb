eee542c2857f44db23d8e54be586971b904cc2d6
Yannick Francois <yannick.francois@free.fr>
2015-05-03 17:20:27 +0200

    Server static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,9 @@ Edgar::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
+  config.assets.enable = true
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
