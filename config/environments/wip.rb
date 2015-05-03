3eb32e48d336e8c085503765936eb604ce01cfdf
Yannick Francois <yannick.francois@free.fr>
2015-05-03 17:46:25 +0200

    Serve static asset everywhere

--- config/environments/wip.rb
+++ config/environments/wip.rb
@@ -8,8 +8,7 @@ Edgar::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -73,7 +72,7 @@ Edgar::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.default :charset => "utf-8"
 
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
