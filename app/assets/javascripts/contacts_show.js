3ca08b71834826db65769613d771db34f9456cde
Yannick Francois <yannick.francois@free.fr>
2015-05-06 10:30:58 +0200

    Fix a quote

--- app/assets/javascripts/contacts_show.js
+++ app/assets/javascripts/contacts_show.js
@@ -11,49 +11,41 @@ function displayMap() {
         type: "GET",
         success: function(data) {
           result = parseGeocodeResult(data.results[0])
-          address_options = { 
-						latitude: result.lat, longitude: result.lng, 
-						admin_name1: result.administrative_area_level_1,
-						admin_name2: result.administrative_area_level_2
-					}
-				 
-					console.log("parsing: " + JSON.stringify(address_options))
+          address_options = {
+            latitude: result.lat, longitude: result.lng,
+            admin_name1: result.administrative_area_level_1,
+            admin_name2: result.administrative_area_level_2
+          }
+
+          console.log("parsing: " + JSON.stringify(address_options))
           mapElement.data("geocodedPrecisely", true)
           jQuery.ajax({
             url: data1.path + "?" + jQuery.param(address_options),
             type: "PUT"
           })
           loadMap(mapElement[0], result.lat, result.lng)
-          
         }
       });
-
     }
     else {
-      if (!mapAlreadyLoaded && data1.latitude && data1.longitude) { 
+      if (!mapAlreadyLoaded && data1.latitude && data1.longitude) {
         loadMap(mapElement[0], data1.latitude, data1.longitude)
-        
       }
-      
     }
-  } 
+  }
 }
 
 function parseGeocodeResult(geocodeResult){
-
   var parsed = {lat: geocodeResult.geometry.location.lat,
     lng: geocodeResult.geometry.location.lng};
+    _addressParts = {administrative_area_level_1: null, administrative_area_level_2: null}
 
-	 _addressParts = {administrative_area_level_1: null, administrative_area_level_2: null}
-  
+    for (var addressPart in _addressParts){
+      parsed[addressPart] = findInfo(geocodeResult, addressPart);
+    }
 
-  for (var addressPart in _addressParts){
-    parsed[addressPart] = findInfo(geocodeResult, addressPart);
-  }
-
-  parsed.type = geocodeResult.types[0];
-
-  return parsed;
+    parsed.type = geocodeResult.types[0];
+    return parsed;
 }
 
 function findInfo(result, type) {
@@ -75,7 +67,7 @@ function loadMap(element, latitude, longitude) {
   map = new google.maps.Map(element, mapOptions)
   marker = new google.maps.Marker({ position: latlng, map: map })
   mapAlreadyLoaded = true
-  
+
 }
 $('a[data-toggle="tab"]').on('shown', function (e) {
   if (e.target.toString().match("#infos") != null) {
