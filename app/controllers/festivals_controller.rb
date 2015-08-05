29958a601539864ce3e8ffd73c665875b722285b
Yannick Francois <yannick.francois@free.fr>
2015-08-05 09:22:58 +0200

    Remove comments

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,18 +1,14 @@
 class FestivalsController < AppController
   include ApplicationHelper
-  # GET /festivals
-  # GET /festivals.json
+
   def index
     @festivals = Festival.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @festivals }
     end
   end
 
-  # GET /festivals/1
-  # GET /festivals/1.json
   def show
     @festival = Festival.find(params[:id])
     add_asset(@festival.structure.contact)
@@ -26,30 +22,22 @@ class FestivalsController < AppController
     @reporting = @festival.structure.reportings.build
   end
 
-  # GET /festivals/new
-  # GET /festivals/new.json
   def new
     @festival = Festival.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @festival }
     end
   end
 
-  # GET /festivals/1/edit
   def edit
     @festival = Festival.find(params[:id])
     add_asset(@festival.structure.contact)
     @festival.schedulings.build(name: "Programmation") unless @festival.schedulings.any?
-  
   end
 
-  # POST /festivals
-  # POST /festivals.json
   def create
     @festival = Festival.new(params[:festival])
-
     respond_to do |format|
       if @festival.save
         format.html { redirect_to @festival, notice: t("activerecord.notices.models.festival.created", name: @festival.name) }
@@ -61,11 +49,8 @@ class FestivalsController < AppController
     end
   end
 
-  # PUT /festivals/1
-  # PUT /festivals/1.json
   def update
     @festival = Festival.find(params[:id])
-    
     respond_to do |format|
       if @festival.update_attributes(params[:festival])
         format.html { redirect_to @festival, notice: t("activerecord.notices.models.festival.updated", name: @festival.name) }
@@ -77,8 +62,6 @@ class FestivalsController < AppController
     end
   end
 
-  # DELETE /festivals/1
-  # DELETE /festivals/1.json
   def destroy
     @festival = Festival.find(params[:id])
     @festival_id = params[:id]
