30b6d8868dfb25592b146541662a237c3858902d
Yannick Francois <yannick.francois@free.fr>
2015-08-07 14:55:47 +0200

    Dont delete all contacts when import

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,49 +6,76 @@ describe Account do
 
   describe :destroy_test_contacts do
     let(:date) { DateTime.new(2014,3,23) }
-    let(:account) { FactoryGirl.create(:account, test_imported_at: date) }
-    it { expect(account.contacts.count).to eq(0) }
 
-    context "with a contact" do
-      let!(:contact) { FactoryGirl.create(:contact, imported_at: date, account: account)}
-      let!(:person) { FactoryGirl.create(:person, account_id: account.id, contact: contact)}
+    context "with a nil test_imported_at" do
+      let(:account) { FactoryGirl.create(:account, test_imported_at: nil) }
 
-      before(:each) do
-        Account.current_id = account
+      context "with a contact" do
+        let!(:contact) { FactoryGirl.create(:contact, imported_at: nil, account: account)}
+        let!(:person) { FactoryGirl.create(:person, account_id: account.id, contact: contact)}
+
+        before(:each) do
+          Account.current_id = account
+        end
+
+        it { expect(account.contacts.count).to eq(1) }
+        it { expect(account.contacts.first).to eq(contact) }
+        it { expect(account.contacts.first.contactable).to eq(person) }
+
+        it "works" do
+          account.destroy_test_contacts
+          expect(account.reload.contacts.count).to eq(1)
+        end
       end
 
-      it { expect(account.contacts.count).to eq(1) }
-      it { expect(account.contacts.first).to eq(contact) }
-      it { expect(account.contacts.first.contactable).to eq(person) }
-
-      it "works" do
-        account.destroy_test_contacts
-        expect(account.reload.contacts.count).to eq(0)
-      end
     end
 
-    context "with a contact that not from last import but an other on date" do
-      let(:date) { DateTime.new(2014,3,23, 10, 22) }
-      let(:later_date) { DateTime.new(2014,3,23, 14, 52) }
-      let!(:contact) { FactoryGirl.create(:contact, imported_at: date, account: account)}
-      let!(:contact_to_remove) { FactoryGirl.create(:contact, imported_at: later_date, account: account)}
+    context "with a test_imported_at" do
+      let(:account) { FactoryGirl.create(:account, test_imported_at: date) }
+      it { expect(account.contacts.count).to eq(0) }
 
-      let!(:person) { FactoryGirl.create(:person, account_id: account.id, contact: contact)}
-      let!(:other_person) { FactoryGirl.create(:person, account_id: account.id, contact: contact_to_remove)}
+      context "with a contact" do
+        let!(:contact) { FactoryGirl.create(:contact, imported_at: date, account: account)}
+        let!(:person) { FactoryGirl.create(:person, account_id: account.id, contact: contact)}
 
-      let(:account) { FactoryGirl.create(:account, test_imported_at: later_date) }
-      before(:each) do
-        Account.current_id = account
+        before(:each) do
+          Account.current_id = account
+        end
+
+        it { expect(account.contacts.count).to eq(1) }
+        it { expect(account.contacts.first).to eq(contact) }
+        it { expect(account.contacts.first.contactable).to eq(person) }
+
+        it "works" do
+          account.destroy_test_contacts
+          expect(account.reload.contacts.count).to eq(0)
+        end
       end
 
-      it { expect(account.contacts.count).to eq(2) }
-      it { expect(account.contacts.sort).to eq([contact, contact_to_remove].sort) }
-      it { expect(account.contacts.map(&:contactable).sort).to eq([person, other_person].sort) }
+      context "with a contact that not from last import but an other on date" do
+        let(:date) { DateTime.new(2014,3,23, 10, 22) }
+        let(:later_date) { DateTime.new(2014,3,23, 14, 52) }
+        let!(:contact) { FactoryGirl.create(:contact, imported_at: date, account: account)}
+        let!(:contact_to_remove) { FactoryGirl.create(:contact, imported_at: later_date, account: account)}
 
-      it "works" do
-        account.destroy_test_contacts
-        expect(account.reload.contacts.count).to eq(1)
-        expect(account.reload.contacts).to eq([contact])
+        let!(:person) { FactoryGirl.create(:person, account_id: account.id, contact: contact)}
+        let!(:other_person) { FactoryGirl.create(:person, account_id: account.id, contact: contact_to_remove)}
+
+        let(:account) { FactoryGirl.create(:account, test_imported_at: later_date) }
+
+        before(:each) do
+          Account.current_id = account
+        end
+
+        it { expect(account.contacts.count).to eq(2) }
+        it { expect(account.contacts.sort).to eq([contact, contact_to_remove].sort) }
+        it { expect(account.contacts.map(&:contactable).sort).to eq([person, other_person].sort) }
+
+        it "works" do
+          account.destroy_test_contacts
+          expect(account.reload.contacts.count).to eq(1)
+          expect(account.reload.contacts).to eq([contact])
+        end
       end
     end
   end
