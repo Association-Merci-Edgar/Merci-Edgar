492d56263de81c2523db05b64a9fbc66cf985339
Christophe Robillard <geek@robiweb.net>
2015-09-03 18:35:41 +0200

    Restrict app access to users with not expired account If account expired, redirect to new subscription

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -11,50 +11,68 @@ describe HomeController do
     end
 
     describe "GET index" do
-      before(:each) { get 'index' }
+      before(:each) { 
+        Account.current_id = user.accounts.first.id 
+        get 'index' 
+      }
       it { expect(response).to redirect_to(welcome_path) }
     end
   end
 
   context "with a logged  user that never see welcome" do
-    let(:user) { FactoryGirl.create(:user, label_name: "truc") }
+    let(:user) { FactoryGirl.create(:admin, label_name: "truc") }
 
     before(:each) do
-      @request.host = 'truc.localhost.com'
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      account = FactoryGirl.build(:account, domain: 'truc')
-      user.accounts = [account]
-      user.save!
-      FactoryGirl.build(:abilitation, user: user, account: account)
       sign_in user
     end
 
     describe "GET index" do
       let!(:pending_task) { FactoryGirl.create(:task) }
-      before(:each) { get 'index' }
+      before(:each) { 
+        Account.current_id = user.accounts.first.id 
+        get 'index' 
+      }
       it { expect(response).to redirect_to(welcome_path) }
     end
   end
 
   context "with a logged  user that already seen welcome" do
-    let(:user) { FactoryGirl.create(:user, label_name: "truc", welcome_hidden: true) }
+    let(:user) { FactoryGirl.create(:admin, label_name: "truc", welcome_hidden: true) }
 
     before(:each) do
-      @request.host = 'truc.localhost.com'
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      account = FactoryGirl.build(:account, domain: 'truc')
-      user.accounts = [account]
-      user.save!
-      FactoryGirl.build(:abilitation, user: user, account: account)
       sign_in user
     end
 
     describe "GET index" do
       let!(:pending_task) { FactoryGirl.create(:task) }
       # TODO WHY WE NEED THIS EMPTY PARAMS ?????
-      before(:each) { get 'index', empty: 1 }
+      before(:each) { 
+        Account.current_id = user.accounts.first.id 
+        get 'index', empty: 1
+      }
       it { expect(response).to be_success }
     end
   end
 
+  context "with a logged  user with an account with subscription lasts soon" do
+    let(:user) { FactoryGirl.create(:admin, with_account_subscription_lasts_soon: true, label_name: "truc") }
+
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+
+    describe "GET index" do
+      let!(:pending_task) { FactoryGirl.create(:task) }
+      before(:each) { 
+        Account.current_id = user.accounts.first.id 
+        get 'index' 
+      }
+      it { expect(response).to redirect_to(welcome_path) }
+      it { expect(flash[:notice]).to eq(I18n.t('notices.subscriptions.need_to_subscribe_soon', end_subscription: I18n.l(user.accounts.first.subscription_lasts_at))) }
+    end
+    
+  end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe WelcomeController do
-
   context "with a logged user" do
 
     let(:user) { FactoryGirl.create(:admin, label_name: "truc") }
@@ -11,13 +10,66 @@ describe WelcomeController do
       sign_in user
     end
 
-    context "with an existing campaign" do
-
-      describe "GET 'index'" do
-        before(:each) { get :index }
-        it { expect(response).to be_success }
-      end
+    describe "GET 'index'" do
+      before(:each) { 
+        Account.current_id = user.accounts.first.id 
+        get :index 
+      }
+      it { expect(response).to be_success }
     end
+  end
+    
+  context "with a logged user with account trial period expired" do
+    let(:user) { FactoryGirl.create(:admin, with_trial_period_account_expired: true, label_name: "truc" ) }
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    describe "GET index" do
+      before(:each) {
+        Account.current_id = user.accounts.first.id 
+        get :index 
+      }
+      it { expect(response).to redirect_to(new_subscription_path) }
+      it { expect(flash[:notice]).to eq(I18n.t('notices.subscriptions.trial_period_ended')) }
+    end
+  end
+
+  context "with a logged user with account subscription not up to date" do
+    let(:user) { FactoryGirl.create(:admin, with_account_subscription_up_to_date: false, label_name: "truc" ) }
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    describe "GET index" do
+      before(:each) {
+        Account.current_id = user.accounts.first.id 
+        get :index 
+      }
+      it { expect(response).to redirect_to(new_subscription_path) }
+      it { expect(flash[:notice]).to eq(I18n.t('notices.subscriptions.not_up_to_date')) }
+    end    
+  end
+
+  context "with a logged user with account subscription last in less than one week" do
+    let(:user) { FactoryGirl.create(:admin, with_account_subscription_up_to_date: false, label_name: "truc" ) }
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+    end
+    describe "GET index" do
+      before(:each) {
+        Account.current_id = user.accounts.first.id 
+        get :index 
+      }
+      it { 
+        account = user.accounts.first
+        puts "------- #{account.last_subscription_at}"
+        puts "------- UP TO DATE #{account.subscription_up_to_date?}"
+        puts "------- TRIAL_PERIOD_ENDED   #{account.trial_period_ended?}"
+        expect(response).to redirect_to(new_subscription_path) }
+      it { expect(flash[:notice]).to eq(I18n.t('notices.subscriptions.not_up_to_date')) }
+    end    
   end
 
 end
