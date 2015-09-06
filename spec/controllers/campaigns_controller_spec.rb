24a200295f4bd3f20b577da3e049a7e40fb94e6c
Yannick Francois <yannick.francois@free.fr>
2015-09-06 15:00:34 +0200

    Add current_account information.

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe CampaignsController, :type => :controller do
 
   context "with a logged user" do
-    let(:user) { FactoryGirl.create(:admin, label_name: "truc") }
+    let(:user) { FactoryGirl.create(:admin) }
 
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in user
+      Account.current_id = user.accounts.first.id
     end
 
     context "with an existing campaign" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe User do
   it "can invite a user if it is the account manager"
   it "can't invite a user unless it is the account manager"  
+
+  describe "have a valid factory" do
+    it { expect(FactoryGirl.build(:user)).to be_valid }
+  end
 end
